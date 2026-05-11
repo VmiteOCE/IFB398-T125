ab283513de5a2e9ee8ff4a431e58a37e6bcd4242
Arabella Rivera <n11622679@qut.edu.au>
2026-05-11 23:16:31 +1000

    Update Home Page

--- client-application/src/components/Footer.jsx
+++ client-application/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="bg-dark text-white py-3">This is a footer</footer>
+        <footer className="bg-dark text-white py-3">
+            © 2025 Rugby Australia. All rights reserved. Part of the Rugby Network
+        </footer>
     );
 }
