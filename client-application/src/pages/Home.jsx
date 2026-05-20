b765567b4c9ff2b5f3fce2339089bcd64fd81111
Arabella Rivera <n11622679@qut.edu.au>
2026-05-20 20:25:16 +1000

    Finished initial Home Page

--- client-application/src/pages/Home.jsx
+++ client-application/src/pages/Home.jsx
@@ -16,9 +16,38 @@ export default function HomePage() {
                     <p className="paragraph-font">Welcome to the Red's Event Recording Application</p>
                     <p className="paragraph-font">To get started, select 'Capture' from the Menu</p>
                 </div>
-                <div style={{backgroundColor: "lightgrey", marginBottom: "10vw"}}>
+                <div style={{backgroundColor: "lightgrey", marginBottom: "10vw", paddingTop: "20px"}}>
                     <h1 style={{alignItems: "center", textAlign: "center"}}>How to Use the App</h1>
-                    <p></p>
+                    <div style={{padding: "10px", paddingRight: "25px"}}>
+                        <ol class = "paragraph-bullet-marks" style={{marginTop:"3vw"}}>
+                            <li><p>Start by <strong>Logging in</strong> through the <strong>'Login' screen</strong></p></li>
+                            <li><p>Select <strong>Starting Zones</strong> by 
+                                either <strong>clicking</strong> on a Zone OR using 
+                                the <strong>arrow keys</strong> &#8594; and &#8592; to move Zones </p>
+                                <ul>
+                                    <li><p>To switch <strong>Defense Zone directions</strong>, press 
+                                        the <strong>switch</strong> button between the <strong>teams</strong></p></li>
+                                    </ul></li>
+                            <li><p>Select <strong>Team</strong> by clicking 
+                                either the <strong>Reds</strong> or <strong>Away</strong> team under the Zones</p></li>
+                            <li><p>Start the <strong>Timer</strong> when the game starts</p>
+                                <ul>
+                                    <li><p>You can <strong>Add</strong> and <strong>Minus 5 Seconds </strong> 
+                                    through the corresponding buttons to Adjust time</p></li>
+                                    <li><p>You can <strong>Pause</strong> and <strong>Start</strong> the
+                                    timer as needed throughout the game using the <strong>Start</strong> button</p></li>
+                                </ul></li>
+                            <li><p>Select an <strong>Action</strong> and it will display in
+                                the <strong>Event History</strong></p>
+                                <ul>
+                                    <li><p>To <strong>Edit</strong> events, press the pencil icon 
+                                        and select a <strong>new</strong> action</p></li>
+                                    <li><p>To <strong>Delete</strong> events, press the bin icon</p></li>
+                                </ul></li>
+                            <li><p>To change keybindings, got to <strong>'Settings'</strong></p></li>
+                            <li><p>To Visualise data, go to <strong>'Data Analysis'</strong></p></li>                                                                        
+                        </ol>
+                    </div>
                 </div>
 
 
