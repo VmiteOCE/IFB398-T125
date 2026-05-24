e0d1c06c6421c0db3b4706e2c3c3ec0c89c6b3bc
Vmite <matthew@datahwy.com>
2026-05-24 21:03:59 +1000

    created Dashboard Page

--- client-application/src/components/Header.jsx
+++ client-application/src/components/Header.jsx
@@ -4,34 +4,11 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className="shadow-sm py-3">
-            <Container>
-
+        <Navbar expand="lg" className="py-2">
+            <Container className="m-0">
                 <Navbar.Brand as={Link} to="/">
-                    Reds App
+                    <img src="favicon.svg" width="100"></img>
                 </Navbar.Brand>
-
-                <Navbar.Toggle aria-controls="main-navbar" />
-
-                <Navbar.Collapse id="main-navbar">
-                    <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to="/">
-                            Home
-                        </Nav.Link>
-
-                        <Nav.Link as={NavLink} to="/login">
-                            Login
-                        </Nav.Link>
-
-                        <Nav.Link as={NavLink} to="/event-capture">
-                            Capture
-                        </Nav.Link>
-
-                        <Nav.Link as={NavLink} to="/game-events">
-                            Game Stats
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
